6c7da46a578ca49ff67b751eced93aedbc2bda97
jakisontraspadini <jakisontraspadini@MacBook-Pro-de-Jakison.local>
2022-07-19 10:08:30 -0300

    aula 1

---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,43 @@
+import { Heading, Icon, useTheme, VStack } from "native-base";
+import { useState } from "react";
+import Logo from "../assets/logo_primary.svg";
+import { Input } from "../Components/Input";
+
+import { Envelope, Key } from "phosphor-react-native";
+import { Button } from "../Components/Button";
+
+export function SignIn() {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const { colors } = useTheme();
+
+  function handleSignIn() {
+    console.log({ email, password });
+  }
+
+  return (
+    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+      <Logo />
+      <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
+        Acesse sua conta
+      </Heading>
+      <Input
+        mb={4}
+        placeholder="E-mail"
+        InputLeftElement={
+          <Icon as={<Envelope color={colors.gray[300]} />} ml={4} />
+        }
+        onChangeText={setEmail}
+      />
+      <Input
+        mb={8}
+        placeholder="Senha"
+        InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
+        secureTextEntry
+        onChangeText={setPassword}
+      />
+      <Button title="Entrar" w="full" onPress={handleSignIn} />
+    </VStack>
+  );
+}
