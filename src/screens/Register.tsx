441ccfc5abb66ed65e8d31aff9bf5da1e2859418
Jakison Traspadini <jakison.traspadini@gmail.com>
2022-08-02 14:11:24 -0300

    aula 3

--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -1,22 +1,69 @@
+import { useState } from "react";
+import { Alert } from "react-native";
 import { VStack } from "native-base";
 import { Button } from "../components/Button";
 import { Header } from "../components/Header";
 import { Input } from "../components/Input";
 
+import firestore from "@react-native-firebase/firestore";
+import { useNavigation } from '@react-navigation/native'
+
 export function Register() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [patrimony, setPatrimony] = useState("");
+  const [description, setDescription] = useState("");
+
+  const navigation = useNavigation();
+
+  function handleNewOrderRegister() {
+    if (!patrimony || !description) {
+      return Alert.alert('Registrar', 'Preencha todos os campos.');
+    }
+
+    setIsLoading(true);
+
+    firestore()
+      .collection('orders')
+      .add({
+        patrimony,
+        description,
+        status: 'open',
+        created_at: firestore.FieldValue.serverTimestamp()
+      })
+      .then(() => {
+        Alert.alert('Solicitação', 'Solicitação registrada com sucesso.');
+        navigation.goBack();
+      })
+      .catch(error => {
+        console.log(error);
+        setIsLoading(false);
+        return Alert.alert('Solicitação', 'Não foi possível registrar o pedido.');
+      })
+  }
+
   return (
     <VStack flex={1} p={6} bg="gray.600">
-      <Header title="Nova solicitação" />
+      <Header title="Solicitação" />
 
-      <Input placeholder="Número do patrimônio" mt={4} />
       <Input
+        onChangeText={setPatrimony}
+        placeholder="Número do patrimônio"
+        mt={4}
+      />
+      <Input
+        onChangeText={setDescription}
         placeholder="Descrição do problema"
         flex={1}
         mt={5}
         multiline
         textAlignVertical="top"
       />
-      <Button title="Cadastrar" mt={5} />
+      <Button
+        isLoading={isLoading}
+        onPress={handleNewOrderRegister}
+        title="Cadastrar"
+        mt={5}
+      />
     </VStack>
   );
 }
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,19 +1,49 @@
+import auth from "@react-native-firebase/auth";
 import { Heading, Icon, useTheme, VStack } from "native-base";
+import { Envelope, Key } from "phosphor-react-native";
 import { useState } from "react";
+import { Alert } from "react-native";
 import Logo from "../assets/logo_primary.svg";
+import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 
-import { Envelope, Key } from "phosphor-react-native";
-import { Button } from "../components/Button";
-
 export function SignIn() {
+  const [isLoading, setIsLoading] = useState(false);
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
 
   const { colors } = useTheme();
 
   function handleSignIn() {
-    console.log({ email, password });
+    if (!email || !password) {
+      return Alert.alert("Entrar", "Informe e-mail e senha.");
+    }
+
+    setIsLoading(true);
+
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(response => {
+        console.log(response)
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+
+        if (error.code === 'auth/invalid-email') {
+          return Alert.alert("Entrar", "E-mail inválido.");
+        }
+
+        if (error.code === 'auth/wrong-password') {
+          return Alert.alert("Entrar", "E-mail ou senha inválido.");
+        }
+
+        if (error.code === 'auth/user-not-found') {
+          return Alert.alert("Entrar", "E-mail ou senha inválido.");
+        }
+
+        return Alert.alert("Entrar", "Não foi possível acessar.");
+      });
   }
 
   return (
@@ -37,7 +67,12 @@ export function SignIn() {
         secureTextEntry
         onChangeText={setPassword}
       />
-      <Button title="Entrar" w="full" onPress={handleSignIn} />
+      <Button
+        isLoading={isLoading}
+        title="Entrar"
+        w="full"
+        onPress={handleSignIn}
+      />
     </VStack>
   );
 }
